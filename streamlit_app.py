4eb0b0db2287045e6eab5ac45f8409314c0b863c
saisudha786 <saisudha9128@gmail.com>
2025-05-01 21:12:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,7 @@ import requests
 
 # Write directly to the app
 st.title(f"Customize Your Smoothie:cup_with_straw:")
-st.write(
-  """Choose the fruits you want in your custom Smoothie!
-  """)
-
+st.write("""Choose the fruits you want in your custom Smoothie!""")
 
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be", name_on_order)
@@ -22,28 +19,22 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 ingredients_list=st.multiselect('Choose up to 5 ingredients:'
                                 ,my_dataframe
                                 ,max_selections=5)
-
 if ingredients_list:
     ingredients_string=''
 
     for fruit_chosen in ingredients_list:
-      ingredients_string+=fruit_chosen + ' '
-      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-      sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-    st.write(ingredients_string)
-
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+        ingredients_string+=fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        st.write(ingredients_string)
+my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
 
-    #st.write(my_insert_stmt)
-    #st.stop()
+#st.write(my_insert_stmt)
+#st.stop()
     
-    time_to_insert=st.button('Submit Order')
+time_to_insert=st.button('Submit Order')
     
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered,'+name_on_order+'!', icon="✅")
-      
-
-      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-      sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+if time_to_insert:
+    session.sql(my_insert_stmt).collect()
+    st.success('Your Smoothie is ordered,'+name_on_order+'!', icon="✅")
